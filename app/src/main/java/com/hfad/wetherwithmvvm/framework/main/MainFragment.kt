fcc9aaa520e61c3b46463c4b69ee73905d4866ba
BikkIlshat <HeavyIlshat@rambler.ru>
2021-08-11 07:28:29 +0300

    initial commit - 3

--- app/src/main/java/com/hfad/wetherwithmvvm/framework/main/MainFragment.kt
+++ app/src/main/java/com/hfad/wetherwithmvvm/framework/main/MainFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
 import com.hfad.wetherwithmvvm.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.*
 
 
 class MainFragment : Fragment() {
@@ -21,9 +24,19 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
+    /*
+    Что бы подписаться на нашу LiveData мы должны сделать Observe (наблюдатель)
+     Observe - будет реагировать на то когда наша LiveData будет изменяться
+
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val observer = Observer<Any> {renderData(it)}
+        viewModel.getData().observe(viewLifecycleOwner, observer) // говорим во viewModel getDat-e  observe (наблюдать) - передаём туда viewLifecycleOwner (это по сути ссылка на нашь MainFragment) и затем передаём наш  observer
+    }
 
+    private fun renderData(data: Any) {
+        Toast.makeText(context, "data", Toast.LENGTH_LONG).show()
     }
 
 
--- app/src/main/java/com/hfad/wetherwithmvvm/framework/main/MainViewModel.kt
+++ app/src/main/java/com/hfad/wetherwithmvvm/framework/main/MainViewModel.kt
@@ -1,7 +1,23 @@
 package com.hfad.wetherwithmvvm.framework.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.lang.Thread.sleep
 
 class MainViewModel : ViewModel() {
+    private val liveDataToObserve: MutableLiveData<Any> = MutableLiveData()
 
+
+    fun getData(): LiveData<Any> {
+        getDataFromLocalSource()
+        return liveDataToObserve
+    }
+
+    private fun getDataFromLocalSource() {
+        Thread {
+            sleep(1000)
+            liveDataToObserve.postValue(Any())
+        }.start()
+    }
 }
