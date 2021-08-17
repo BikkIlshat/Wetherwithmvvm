b200d7382fb1dc0dc2e53c64aed778a3a460af10
BikkIlshat <HeavyIlshat@rambler.ru>
2021-08-17 19:49:48 +0300

    initial commit - рефакторинг кода

--- app/src/main/java/com/hfad/wetherwithmvvm/model/repository/RepositoryImpl.kt
+++ app/src/main/java/com/hfad/wetherwithmvvm/model/repository/RepositoryImpl.kt
@@ -1,15 +1,15 @@
 package com.hfad.wetherwithmvvm.model.repository
 
+import com.hfad.wetherwithmvvm.model.entities.City
 import com.hfad.wetherwithmvvm.model.entities.Weather
-import com.hfad.wetherwithmvvm.model.entities.getRussianCities
-import com.hfad.wetherwithmvvm.model.entities.getWorldCities
+
 
 class RepositoryImpl: Repository {
 
     override fun getWeatherFromServer() = Weather()
 
-    override fun getWeatherFromLocalStorageRus() = getRussianCities()
+    override fun getWeatherFromLocalStorageRus() = City.getRussianCities()
 
-    override fun getWeatherFromLocalStorageWorld() = getWorldCities()
+    override fun getWeatherFromLocalStorageWorld() = City.getWorldCities()
 }
 
