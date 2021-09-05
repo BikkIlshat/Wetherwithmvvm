e16d7880bfceedbc2c378573e3bdaa6bd56b185b
BikkIlshat <HeavyIlshat@rambler.ru>
2021-09-05 19:30:37 +0300

    Commit - 4

--- app/src/main/java/com/hfad/wetherwithmvvm/framework/ui/maps/MapsFragment.kt
+++ app/src/main/java/com/hfad/wetherwithmvvm/framework/ui/maps/MapsFragment.kt
@@ -1,35 +1,68 @@
 package com.hfad.wetherwithmvvm.framework.ui.maps
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.graphics.Color
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.Transformations.map
 
 import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.hfad.wetherwithmvvm.R
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.PolylineOptions
+import com.hfad.wetherwithmvvm.databinding.FragmentMapsBinding
 
 class MapsFragment : Fragment() {
+    private lateinit var map: GoogleMap
+    private var menu: Menu? = null
+    private val markers: ArrayList<Marker> = ArrayList()
+    private var _binding: FragmentMapsBinding? = null
+    private val binding get() = _binding!!
 
+
+
+
+
+    @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
+        map = googleMap
 
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map.uiSettings.isZoomControlsEnabled = true
+        map.uiSettings.isMyLocationButtonEnabled = true
+
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            map.isMyLocationEnabled = true
+        }
+
+        val initialPlace = LatLng(44.952117, 34.102417)
+        val marker = googleMap.addMarker(
+            MarkerOptions().position(initialPlace).title(getString(R.string.start_marker))
+        )
+        marker?.let { markers.add(it) }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(initialPlace))
+        googleMap.setOnMapLongClickListener { latLng ->
+            setMarker(latLng, "From long click")
+            drawLine()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -37,13 +70,73 @@ class MapsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+        _binding = FragmentMapsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    override fun onDestroyView() {
+        menu?.findItem(R.id.menu_google_maps)?.isVisible = true
+        _binding = null
+        super.onDestroyView()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.map_menu, menu)
+        this.menu = menu
+        menu.findItem(R.id.menu_google_maps).isVisible = false
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_map_mode_normal -> {
+                map.mapType = GoogleMap.MAP_TYPE_NORMAL
+                return true
+            }
+            R.id.menu_map_mode_satellite -> {
+                map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+                return true
+            }
+            R.id.menu_map_mode_terrain -> {
+                map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+                return true
+            }
+            R.id.menu_map_traffic -> {
+                map.isTrafficEnabled = !map.isTrafficEnabled
+                return true
+            }
+        }
+
+        return false
+    }
+
+
+    private fun setMarker(location: LatLng, searchText: String) {
+        map.addMarker(
+            MarkerOptions()
+                .position(location)
+                .title(searchText)
+        )?.let { markers.add(it) }
+    }
+
+    private fun drawLine() {
+        val last: Int = markers.size - 1
+        if (last >= 1) {
+            val previous: LatLng = markers[last - 1].position
+            val current: LatLng = markers[last].position
+            map.addPolyline(
+                PolylineOptions()
+                    .add(previous, current)
+                    .color(Color.RED)
+                    .width(5f)
+            )
+        }
     }
 
     companion object {
