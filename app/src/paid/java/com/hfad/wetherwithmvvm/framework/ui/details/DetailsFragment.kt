ec1a9f72c3f415b510c20ef59a96e35c6cbd1328
BikkIlshat <HeavyIlshat@rambler.ru>
2021-09-12 21:31:00 +0300

    HW-11 добавил в Git файлы  которые слетели при создании 2х версий free/paid

---
+++ app/src/paid/java/com/hfad/wetherwithmvvm/framework/ui/details/DetailsFragment.kt
@@ -0,0 +1,86 @@
+package com.hfad.wetherwithmvvm.framework.ui.details
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.hfad.wetherwithmvvm.AppState
+import com.hfad.wetherwithmvvm.R
+import com.hfad.wetherwithmvvm.databinding.DetailsFragmentBinding
+import com.hfad.wetherwithmvvm.model.entities.Weather
+import com.squareup.picasso.Picasso
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class DetailsFragment : Fragment() {
+
+    private var _binding: DetailsFragmentBinding? = null
+    private val binding get() = _binding!!
+    private val viewModel: DetailsViewModel by viewModel()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DetailsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val weather = arguments?.getParcelable<Weather>(BUNDLE_EXTRA)
+        weather?.let {
+            with(binding) {
+                cityName.text = it.city.city
+                cityCoordinates.text = String.format(
+                    getString(R.string.city_coordinates),
+                    it.city.lat.toString(),
+                    it.city.lon.toString()
+                )
+                viewModel.liveDataObserver.observe(viewLifecycleOwner, { appState ->
+                    when (appState) {
+                        is AppState.Error -> {
+                            mainView.visibility = View.INVISIBLE
+                            loadingLayout.visibility = View.GONE
+                            errorTV.visibility = View.VISIBLE
+                        }
+                        AppState.Loading -> {
+                            mainView.visibility = View.INVISIBLE
+                            binding.loadingLayout.visibility = View.VISIBLE
+                        }
+                        is AppState.Success -> {
+                            loadingLayout.visibility = View.GONE
+                            mainView.visibility = View.VISIBLE
+                            temperatureValue.text = appState.weatherData[0].temperature.toString()
+                            feelsLikeValue.text = appState.weatherData[0].feelsLike.toString()
+                            weatherCondition.text = appState.weatherData[0].condition
+                        }
+                    }
+                })
+                viewModel.loadData(it.city.city, it.city.lat, it.city.lon)
+                Picasso
+                    .get()
+                    .load("https://freepngimg.com/thumb/city/36275-3-city-hd.png")
+                    .fit()
+                    .into(imageView)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    companion object {
+        const val BUNDLE_EXTRA = "weather"
+
+        fun newInstance(bundle: Bundle): DetailsFragment {
+            val fragment = DetailsFragment()
+            fragment.arguments = bundle
+            return fragment
+        }
+
+    }
+}
---
+++ app/src/paid/java/com/hfad/wetherwithmvvm/model/repository/RepositoryImpl.kt
@@ -0,0 +1,55 @@
+package com.hfad.wetherwithmvvm.model.repository
+
+
+import com.hfad.wetherwithmvvm.model.database.Database
+import com.hfad.wetherwithmvvm.model.database.HistoryEntity
+import com.hfad.wetherwithmvvm.model.entities.City
+import com.hfad.wetherwithmvvm.model.entities.Weather
+import com.hfad.wetherwithmvvm.model.rest.WeatherRepo
+
+private const val LATITUDE = 0.0
+private const val LONGITUDE = 0.0
+private const val FEELSLIKE = 0
+private const val HISTORY_ENTITY_ID = 0
+
+
+class RepositoryImpl : Repository {
+
+    override fun getWeatherFromServer(lat: Double, lng: Double): Weather {
+        val dto = WeatherRepo.api.getWeather(lat, lng).execute().body()
+
+        return Weather(
+
+            temperature = dto?.fact?.temp ?: 0,
+            feelsLike = dto?.fact?.feelsLike ?: 0,
+            condition = dto?.fact?.condition
+        )
+    }
+
+    override fun getWeatherFromLocalStorageRus() = City.getRussianCities()
+
+    override fun getWeatherFromLocalStorageWorld() = City.getWorldCities()
+
+    override fun getAllHistory(): List<Weather> =
+        convertHistoryEntityToWeather(Database.db.historyDao().all())
+
+
+    override fun saveEntity(weather: Weather) {
+        Database.db.historyDao().insert(convertWeatherToEntity(weather))
+    }
+
+    private fun convertHistoryEntityToWeather(entityList: List<HistoryEntity>): List<Weather> =
+        entityList.map {
+            Weather(City(it.city, LATITUDE, LONGITUDE), it.temperature, FEELSLIKE, it.condition)
+        }
+
+
+    private fun convertWeatherToEntity(weather: Weather): HistoryEntity =
+        HistoryEntity(
+            HISTORY_ENTITY_ID.toLong(), weather.city.city,
+            weather.temperature ?: 0,
+            weather.condition ?: ""
+        )
+}
+
+
